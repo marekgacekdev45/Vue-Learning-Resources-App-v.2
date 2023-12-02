0fea8d12d7e4060c7589d1477f128cda53fe99dd
gacmen45 <marekgacek45@gmail.com>
2023-12-02 13:46:47 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <h1>test</h1>
+  <stored-resources :resources="storedResources"></stored-resources>
+</template>
+
+<script>
+import StoredResources from './components/learning-resources/StoredResources.vue';
+
+
+export default {
+  components: { StoredResources },
+  
+  data() {
+    return {
+      storedResources: [
+        {
+          id: '1',
+          title: 'Official Guide',
+          description: 'lorem ipsum dolor sit amet',
+          link: 'https://vue.org',
+        },
+        {
+          id: '2',
+          title: 'google',
+          description: 'lorem ipsum dolor sit amet',
+          link: 'https://google.com',
+        },
+        {
+          id: '3',
+          title: 'udemy',
+          description: 'lorem ipsum dolor sit amet',
+          link: 'https://udemy.com',
+        },
+      ],
+    };
+  },
+};
+</script>
---
+++ src/components/learning-resources/LearningResource.vue
@@ -0,0 +1,69 @@
+<template>
+    <li>
+        <div>
+            <header>
+                <h3>{{ title }}</h3>
+                <button>delete</button>
+            </header>
+        </div>
+        <p>{{ description }}</p>
+        <nav>
+            <a :href="link">View Resource</a>
+        </nav>
+    </li>
+</template>
+
+<script>
+export default {
+    props:['title','description','link']
+}
+</script>
+
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Roboto', sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
+
+<style scoped>
+li {
+  margin: auto;
+  max-width: 40rem;
+}
+
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+h3 {
+  font-size: 1.25rem;
+  margin: 0.5rem 0;
+}
+
+p {
+  margin: 0.5rem 0;
+}
+
+a {
+  text-decoration: none;
+  color: #ce5c00;
+}
+
+a:hover,
+a:active {
+  color: #c89300;
+}
+</style>
